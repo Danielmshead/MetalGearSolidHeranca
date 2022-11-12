4ad1353ded4295eb68af2ce6da508539e53b8def
Daniel Matias da Silva <daniel.silva@leanwork.com.br>
2022-11-12 11:53:13 -0300

    Adicionar arquivos de projeto.

---
+++ MetalGearSolidHeranca/ContaEstudante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetalGearSolidHeranca
+{
+    class ContaEstudante : ContaComum
+    {
+        public int Milhas { get; private set; }
+
+        public override void Deposita(double valor)
+        {
+            base.Deposita(valor);
+            this.Milhas += (int)valor;
+            throw new Exception("Uma conta de estudante não possui rendimentos");
+        }
+    }
+}
---
+++ MetalGearSolidHeranca/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace MetalGearSolidHeranca
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<ContaComum> contas = ContasDoBanco();
+
+            foreach(ContaComum conta in contas) 
+            {
+                conta.somaInvestimento();
+
+                Console.WriteLine("Novo Saldo :" + conta.Saldo);
+            
+            }
+
+            Console.ReadLine();
+        }
+
+        private static IList<ContaComum> ContasDoBanco() 
+        {
+            List<ContaComum> contas = new List<ContaComum>();
+            contas.Add(umaContaComum(100));
+            contas.Add(umaContaComum(150));
+            contas.Add(umaContaEstudante(100));
+            return contas;
+        }
+
+        private static ContaEstudante umaContaEstudante(double saldo)
+        {
+            ContaEstudante conta = new ContaEstudante();
+            conta.Deposita(saldo);
+            return conta;
+        }
+
+        private static ContaComum umaContaComum(double saldo)
+        {
+            ContaComum conta = new ContaComum();
+            conta.Deposita(saldo);
+            return conta;
+        }
+    }
+}
